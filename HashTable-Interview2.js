7a2d668534084b72c5c0619998beae6f30266a6e
Pradeep Reddy Palavalli <pradeepreddypalavalli1999@gmail.com>
2022-02-04 09:34:55 +0530

    Graph Data Structure added

---
+++ HashTable-Interview2.js
@@ -0,0 +1,17 @@
+// check if both array have common elements in them. It is O(n) complex.
+function itemInCommon(arr1, arr2) {
+     let obj = {}
+     for (let i = 0; i < arr1.length; i++) {
+         obj[arr1[i]] = true
+     }
+     for (let j = 0; j < arr2.length; j++) {
+       if (obj[arr2[j]]) return true
+     }
+     return false
+}
+   
+   
+let array1 = [1, 3, 5]
+let array2 = [2, 4, 5]
+   
+itemInCommon(array1, array2)
